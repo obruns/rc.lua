2a94573b09af24067cab53de61b53366b82f4b38
Oliver Bruns <obruns@gmail.com>
2013-05-12 11:14:00 +0200

    awesome: use the '[' key in favor of the Escape key to switch to last tag
    In an attempt to teach myself to not use the Escape key anymore because
    it is located in an almost unreachable position on modern keyboards an
    might cause health-issues in the long term I map all usages to more
    appropriate keys.
    
    In vim Ctrl-[ is the same as <Esc>, therefore use Mod4-[ in awesome

--- rc.lua
+++ rc.lua
@@ -287,7 +287,7 @@ root.buttons(awful.util.table.join(
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey,           }, "[", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
         function ()
